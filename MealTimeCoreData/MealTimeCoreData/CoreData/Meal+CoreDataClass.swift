761111948e1de370d7d12759d0d964f05945fee2
slemeshaev <lemeshaevs@gmail.com>
2021-03-08 21:11:33 +0300

    files for coredata

---
+++ MealTimeCoreData/MealTimeCoreData/CoreData/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTimeCoreData
+//
+//  Created by Станислав Лемешаев on 08.03.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
