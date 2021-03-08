2fa4df268e94952bcf15cdcc852651e24051ba29
slemeshaev <lemeshaevs@gmail.com>
2021-03-08 15:23:31 +0300

    files for project

---
+++ MealTimeCoreData/MealTimeCoreData/MealTimeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTimeCoreData
+//
+//  Created by Станислав Лемешаев on 08.03.2021.
+//
+
+import UIKit
+
+class MealTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
